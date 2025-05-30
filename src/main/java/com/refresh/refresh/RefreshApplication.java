b8044f5e77c47cc9ac53e6fb3d95738fb96b2392
ildanpuma <heje0623@naver.com>
2025-05-30 14:45:56 +0900

    api테스트 기능 구현
    Signed-off-by: ildanpuma <heje0623@naver.com>

--- src/main/java/com/refresh/refresh/RefreshApplication.java
+++ src/main/java/com/refresh/refresh/RefreshApplication.java
@@ -2,8 +2,12 @@ package com.refresh.refresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.refresh.refresh.repository")
+@EntityScan(basePackages = "com.refresh.refresh.entity")
 public class RefreshApplication {
 
     public static void main(String[] args) {
