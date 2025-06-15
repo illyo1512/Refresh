c3116404d78a5f6f14cdbbcbdeac758fa0bd9f66
illyo1512 <103218354+illyo1512@users.noreply.github.com>
2025-06-15 19:33:43 +0900

    Delete model/model_loader.py

--- model/model_loader.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
-
-# 모델 로딩
-model_name = "monologg/koelectra-base-v3-naver-ner"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForTokenClassification.from_pretrained(model_name)
-
-# 파이프라인 생성
-ner = pipeline("ner", model=model, tokenizer=tokenizer, aggregation_strategy="none")
